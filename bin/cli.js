2e9f4359cbc116a1d454cae9937424213744f78c
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-08-23 04:26:43 +0530

    Add initial project structure with CLI and template files of express-modular-monolith package

---
+++ bin/cli.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { exec } from "node:child_process";
+import fs from "node:fs/promises";
+import { input, select } from "@inquirer/prompts";
+
+
+const projectName = await input({
+  message: "Project name:"
+});
+
+const language = await select({
+  message: "Choose a language:",
+  choices: [
+    {
+      name: "JavaScript",
+      value: "javascript"
+    },
+    {
+      name: "TypeScript",
+      value: "typescript"
+    }
+  ]
+});
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const templatePath = path.join(__dirname, "..", "templates", language);
+
+const projectPath = projectName === "." ? process.cwd() : path.join(process.cwd(), projectName);
+await fs.cp( templatePath, projectPath, { recursive: true });
+
+const packageJsonPath = path.join(projectPath, "package.json");
+
+const packageJson = JSON.parse(
+  await fs.readFile(packageJsonPath, "utf-8")
+);
+
+if (projectName === ".") {
+  packageJson.name = path.basename(process.cwd());
+}else{
+  packageJson.name = projectName;
+}
+
+await fs.writeFile(
+  packageJsonPath,
+  JSON.stringify(packageJson, null, 2)
+);
+
+await exec("npm install", {
+  cwd: projectPath
+})
+
+
