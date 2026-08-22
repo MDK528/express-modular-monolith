bc561c1d59e62a86f73d0ea50f7b8e7fcd8721e1
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-08-23 04:47:39 +0530

    Add initial TypeScript app, server, and database connection placeholder files for typescript template

---
+++ templates/typescript/src/server.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import app from './app.js'
+
+
+const PORT = process.env.PORT || 8000
+
+;(async function start () {
+    try {
+        // execute database connection function here
+        app.listen(PORT, ()=>{
+            console.log(`Server is running at port ${PORT}`)
+        })
+    } catch (error) {
+        console.error('Failed to start server', error)
+        process.exit(1)
+    }
+})()
